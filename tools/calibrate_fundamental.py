75fd1d0312ee8d7b5878282662cd4423afd74b3b
furaga <furaga.fukahori@gmail.com>
2022-03-21 16:37:39 +0900

    wip

--- tools/calibrate_fundamental.py
+++ tools/calibrate_fundamental.py
@@ -5,6 +5,8 @@ import cv2
 import glob
 from pathlib import Path
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
 
 import geometry
 import calib.result
@@ -57,11 +59,57 @@ def recoverPose(E, points1, points2, mtx):
     return True, best_Rt[0], best_Rt[1]
 
 
+def draw_points3d(points3d, colors, world_size=0.5, center=None, block=True, s=8):
+    points3d = np.array(points3d)
+    colors = np.array(colors)
+
+    fig = plt.figure(figsize=(9, 9))
+    ax = fig.add_subplot(111, projection="3d")
+    mean = center if center is not None else np.mean(points3d, axis=0)
+    x = points3d[:, 0] - mean[0]
+    y = points3d[:, 1] - mean[1]
+    z = points3d[:, 2] - mean[2]
+
+    ax.scatter(x, y, z, c=colors, s=s)
+    ax.set_title("3D Points")
+
+    ax.set_xlim(-world_size, world_size)
+    ax.set_ylim(-world_size, world_size)
+    ax.set_zlim(-world_size, world_size)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    plt.show(block=block)
+
+
+def make_view_position(R, t):
+    p = -np.matmul(R, t.T).T
+    return p
+
+
+def make_Rt(R, t, expand=False):
+    Rt = np.zeros((4 if expand else 3, 4), float)
+    Rt[:3, :3] = R
+    Rt[:3, 3] = t.ravel()
+    if expand:
+        Rt[3, 3] = 1
+    return Rt
+
+
 def main(args):
     args.out_dir.mkdir(exist_ok=True)
 
     all_ids = calib.result.all_ids
     all_cam_ids = sorted(set([c for _, c in all_ids]))
+    cam_id_colors = {
+        c: (
+            random.random(),
+            random.random(),
+            random.random(),
+        )
+        for c in all_cam_ids
+    }
 
     # frame -> Dict[str, List[Point]]
     frame_to_dict = {}
@@ -88,6 +136,7 @@ def main(args):
 
     mtx = calib.result.camera_matrix
     distort = calib.result.distort
+    Rt_dict = {}
     for (cam_id1, cam_id2), (pts1, pts2) in corr.items():
         pts1 = np.reshape(pts1, (-1, 1, 2))
         pts2 = np.reshape(pts2, (-1, 1, 2))
@@ -95,12 +144,57 @@ def main(args):
         pts2_undist = geometry.undistort(np.array(pts2), mtx, distort)
 
         E, inliers = cv2.findEssentialMat(pts1_undist, pts2_undist, mtx)
-        #print(inliers)
+        inliers = inliers.ravel()
+        pts1_undist = [p for f, p in zip(inliers, pts1_undist) if f >= 1]
+        pts2_undist = [p for f, p in zip(inliers, pts2_undist) if f >= 1]
+
+        # print(inliers)
         ret, R, t = recoverPose(E, pts1_undist, pts2_undist, mtx)
         if not ret:
             print("failed", (cam_id1, cam_id2))
 
-        print("OK", (cam_id1, cam_id2), R, t)
+        Rt_dict[(cam_id1, cam_id2)] = R, t
+
+    #
+    # Visualize
+    #
+    points3d = [np.zeros(3, float)]
+    colors = [cam_id_colors[all_cam_ids[0]]]
+
+    # print(list(edges.keys()))
+    all_Rt = {}
+    all_Rt[all_cam_ids[0]] = np.eye(4)
+
+    for i in range(1, len(all_cam_ids)):
+        cam_id2 = all_cam_ids[i]
+        global_Rts = []
+        for j in range(i):
+            if j != 0:
+                continue
+            cam_id1 = all_cam_ids[j]
+            key = cam_id1, cam_id2
+
+            if key in Rt_dict:
+                M0 = all_Rt[all_cam_ids[0]]
+                Mj = all_Rt[all_cam_ids[j]]
+                base = np.matmul(M0, np.linalg.inv(Mj))
+
+                R, t = Rt_dict[key]
+                Rt = make_Rt(R, t, expand=True)
+
+                M = np.matmul(base, Rt)  # M0Mi
+                global_R = M[:3, :3]
+                global_t = M[:3, 3]
+
+                pt3d = make_view_position(global_R, global_t)
+                points3d.append(pt3d)
+                colors.append(cam_id_colors[all_cam_ids[i]])
+                global_Rts.append(make_Rt(global_R, global_t, expand=True))
+
+        all_Rt[all_cam_ids[i]] = np.mean(global_Rts, axis=0)
+
+    print(points3d)
+    draw_points3d(points3d, colors, world_size=1.5, center=(0, 0, 0), block=True, s=64)
 
 
 if __name__ == "__main__":
